495cbb7c9f024cb9f9cb396be283a0c32a14de8a
Jane Moskalenko <jane.moskalenko@gmail.com>
2017-07-05 20:28:03 +0300

    Log

--- src/main/Controllers/LoginServlet.java
+++ src/main/Controllers/LoginServlet.java
@@ -1,16 +1,22 @@
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class LoginServlet extends HttpServlet {
+    final static Logger LOGGER = Logger.getLogger(LoginServlet.class);
+    static{
+        BasicConfigurator.configure();
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-     //   resp.sendRedirect("https://google.com");
         String login = req.getParameter("login");
         String password = req.getParameter("password");
         if (AutorizationService.auth(login, password)) {
@@ -27,6 +33,7 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+        LOGGER.debug("Request for login page");
         resp.getWriter().println("Hello world");
         request.getRequestDispatcher("/jsp_login.jsp").forward(request, resp);
     }
