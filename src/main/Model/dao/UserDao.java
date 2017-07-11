bfaadaaf2000cd7f9d80bd0161226b8fa53e1c73
Jane Moskalenko <jane.moskalenko@gmail.com>
2017-07-11 12:32:12 +0300

    Table of user list. Delete and add users

--- src/main/Model/dao/UserDao.java
+++ src/main/Model/dao/UserDao.java
@@ -7,15 +7,16 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by admin on 01.07.2017.
  */
 public class UserDao {
+    private static Connection connection = ConnectionManager.getConnection();
 
     public static User getUserByLoginAndPassword(String login, String password) {
-
-        Connection connection = ConnectionManager.getConnection();
         User user = null;
 
         try (Statement statement = connection.createStatement()) {
@@ -28,11 +29,63 @@ public class UserDao {
                 user = new User(resultSet.getString(1),
                         resultSet.getString("password"));
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
         return user;
+    }
+
+    public static HashMap<String, String> getAllUser(){
+        HashMap<String, String> hmapUsers = new HashMap<>();
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery
+                    ("SELECT * FROM public.users");
+            while (resultSet.next()){
+                hmapUsers.put(resultSet.getString("login"),
+                        resultSet.getString("password"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return hmapUsers;
+    }
+
+    public static ArrayList<User> getAllUserInObject(){
+        ArrayList<User> userArrayList = new ArrayList<User>();
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery
+                    ("SELECT * FROM public.users");
+            while (resultSet.next()){
+                User newUser = new User(resultSet.getString("login"),
+                        resultSet.getString("password"));
+                userArrayList.add(newUser);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userArrayList;
+    }
+
+    public static boolean deleteUserByLogin(String login){
+        try (Statement statement = connection.createStatement()) {
+            String sql = "DELETE FROM public.users " +
+                    "WHERE login = '" + login + "'";
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public static boolean addUser(String login, String password){
+        try (Statement statement = connection.createStatement()) {
+            String sql = "INSERT INTO public.users (login, password)" +
+                    "VALUES ('" + login + "', '" + password + "')";
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 }
